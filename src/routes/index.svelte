002c5555e0fe2e23a1a0ebe6e01b816925ee5a6e
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-08-11 11:31:24 +0200

    change scroll behaviour

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,12 @@
 	const speeds = [-1000, -100, -50, -25, -10, -5, -1, 0, 1, 5, 10, 25, 50, 100, 1000];
 	let speed = 0;
 
+	let behavior = 'smooth';
+
+	const toggleBehavior = () => {
+		behavior = behavior === 'smooth' ? 'auto' : 'smooth';
+	};
+
 	const setSpeed = (newSpeed) => {
 		speed = newSpeed;
 	};
@@ -14,18 +20,23 @@
 		setInterval(() => {
 			window.scrollBy({
 				top: speed,
-				behavior: 'smooth'
+				behavior: behavior
 			});
 		}, 10);
 	});
 </script>
 
 <div class="sticky top-0">
-	{#each speeds as speed}
-		<button class="border mx-4 my-2 bg-indigo-200" on:click={() => setSpeed(speed)}
-			>Speed {speed}</button
-		>
-	{/each}
+	<button class="border px-4 py-2 bg-indigo-100 mb-3" on:click={() => toggleBehavior()}
+		>Toggle behavior: now {behavior}</button
+	>
+	<div class="flex gap-2 flex-wrap">
+		{#each speeds as speed}
+			<button class="border px-4 py-2 bg-indigo-100" on:click={() => setSpeed(speed)}
+				>Speed {speed}</button
+			>
+		{/each}
+	</div>
 </div>
 <h1 class="text-3xl font-bold underline">Scrolling</h1>
 
